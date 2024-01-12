f50de1cfa7674fd12fcde0fa0718c44bd6139daf
Sai Aung Hlyan Htet <saiaunghlyanhtet21303@gmail.com>
2024-01-12 17:35:03 +0630

    feat: test pipeline

--- src/main/java/com/sahh/ekscicd/TestController.java
+++ src/main/java/com/sahh/ekscicd/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("hello")
     public String hello(){
-        return "hello from testing cicd with eks updated";
+        return "hello from testing cicd (latest version)";
     }
 }
